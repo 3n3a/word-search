d999b76fba7ab2e5e9c1fd27da3a9fe45fe27d08
3n3a <info@3n3a.ch>
2023-01-14 17:36:21 +0100

    fix layouting words

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -5,7 +5,7 @@ import type { PageData } from './$types';
     export let data: PageData;
 </script>
 
-<div class="flex flex-row gap-2 justify-between content-center my-8">
+<div class="flex md:flex-row flex-col gap-2 justify-between content-center my-8">
     <a href="/" class="btn btn-accent">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -13,7 +13,7 @@ import type { PageData } from './$types';
         Back
     </a>
     
-    <div class="flex flex-row gap-4">
+    <div class="flex md:flex-row flex-col gap-4">
         <h2 class="subtitle">{data.titlePrefix}</h2>
 
         <div class="flex flex-row gap-1">
